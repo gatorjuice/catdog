0c925c632b10074bf05d5e195259166097b85639
James Gates <gatorjuice@gmail.com>
2019-03-10 18:19:21 -0500

    made sentence more complicated

--- lib/sentence_builder.rb
+++ lib/sentence_builder.rb
@@ -4,17 +4,50 @@ class SentenceBuilder
   attr_reader :words
 
   def initialize(options)
-    @word_count = options[:word_count]
-    @words = []
   end
 
-  def build
-    @word_count.times do
-      @words << RandomWord.nouns.next.gsub('_', ' ')
-    end
+  def sentence
+    "#{subjects.sample} the #{adjective} and, #{adjective} #{noun}," \
+    "#{second_phrase_starts.sample}, the #{noun}, has #{adjective} #{noun}"
   end
 
   def speak
-    `say #{@words.join(' ')}`
+    `say "#{sentence}"`
+  end
+
+  private
+
+  def noun
+    RandomWord.nouns.next.gsub('_', ' ')
+  end
+
+  def adjective
+    RandomWord.adjs.next.gsub('_', ' ')
+  end
+
+  def subjects
+    [
+      'I love',
+      'I want',
+      'where is',
+      'please help me find',
+      'cats love',
+      'dogs love',
+      'so many lusting after',
+      'my mother hates',
+      'my father wanted',
+      'my cousing takes',
+      'all of my friends prefer',
+      'your favorite part of living is'
+    ].map{ |phrase| phrase.gsub(' ', ', ')}
+  end
+
+  def second_phrase_starts
+    [
+      'becaws',
+      'unless',
+      'assuming',
+
+    ]
   end
 end
--- modes/say_stuff.rb
+++ modes/say_stuff.rb
@@ -3,10 +3,8 @@ require_relative '../lib/sentence_builder'
 class SayStuff
   def self.start
     loop do
-      sentence = SentenceBuilder.new(word_count: 10)
-      sentence.build
-      sentence.speak
-      sleep 10
+      SentenceBuilder.new(word_count: 10).speak
+      sleep 2
     end
   end
 end
