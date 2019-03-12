55a8e4c1a87ae98269d2a426785c2c7504a8c9bf
James Gates <gatorjuice@gmail.com>
2019-03-12 00:07:15 -0500

    added music

--- modes/say_stuff.rb
+++ modes/say_stuff.rb
@@ -1,9 +1,14 @@
 require_relative '../lib/sentence_builder'
+require 'audio-playback'
 
 class SayStuff
   def self.start
     loop do
+      @playback = AudioPlayback.play("/home/pi/songs/#{('a'..'t').to_a.sample}.wav")
+      @playback.block
+
       SentenceBuilder.new.speak
+
       sleep rand(60)
     end
   end
