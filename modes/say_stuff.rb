9cdfedf9764641c927be8bd1940f5c62f126c39b
James Gates <gatorjuice@gmail.com>
2019-03-17 20:16:51 -0500

    changed location

--- modes/say_stuff.rb
+++ modes/say_stuff.rb
@@ -4,7 +4,7 @@ require 'audio-playback'
 class SayStuff
   def self.start
     loop do
-      @playback = AudioPlayback.play("/Users/jgates/Downloads/#{('a'..'t').to_a.sample}.wav")
+      @playback = AudioPlayback.play("/home/pi/songs/#{('a'..'t').to_a.sample}.wav")
       @playback.block
 
       SentenceBuilder.new.speak
